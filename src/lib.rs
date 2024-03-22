bab123355730611b9a02bad5c56a9bdabe2e6ed2
Mees Delzenne <mees.delzenne@gmail.com>
2024-03-22 10:19:20 +0100

    Remove doc-cfg feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(feature = "doc-cfg", feature(doc_cfg))]
+#![cfg_attr(docrs, feature(doc_cfg))]
 
 mod allocator;
 pub mod stack;
@@ -6,12 +6,12 @@ mod stub_ctx;
 mod task;
 
 #[cfg(feature = "tree")]
-#[cfg_attr(feature = "doc-cfg", doc(cfg(feature = "tree")))]
+#[cfg_attr(docrs, doc(cfg(feature = "tree")))]
 pub mod tree;
 
 #[cfg(feature = "tree")]
 #[doc(inline)]
-#[cfg_attr(feature = "doc-cfg", doc(cfg(feature = "tree")))]
+#[cfg_attr(docrs, doc(cfg(feature = "tree")))]
 pub use tree::{Stk as TreeStk, TreeStack};
 
 #[doc(inline)]
